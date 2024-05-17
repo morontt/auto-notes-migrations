12b70f0afaf4426210e0a3587643bd3d2aa1a629
Alexander Kharchenko <morontt@yandex.ru>
2024-05-17 21:26:45 +0300

    small change SQL query

--- sql/fuels.sql
+++ sql/fuels.sql
@@ -3,8 +3,10 @@ SELECT
     f.value,
     azs.name AS station,
     f.cost,
-    c.model_name AS car
+    c.model_name AS car,
+    m.distanse
 FROM fuels AS f
 INNER JOIN filling_stations AS azs ON f.station_id = azs.id
 INNER JOIN cars AS c ON f.car_id = c.id
+LEFT JOIN mileages AS m ON f.mileage_id = m.id
 ORDER BY f.id;
