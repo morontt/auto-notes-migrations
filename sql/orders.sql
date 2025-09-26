27ec01727f3b65a966c86ddc26ca11e9b1983c60
Alexander Kharchenko <morontt@yandex.ru>
2025-09-26 09:05:41 +0300

    change orders SQL-query

--- sql/orders.sql
+++ sql/orders.sql
@@ -5,8 +5,12 @@ SELECT
     o.description,
     m.distance,
     o.used_at,
-    o.capacity
+    o.capacity,
+    o.car_id,
+    c.brand_name,
+    c.model_name
 FROM orders AS o
 LEFT JOIN order_types AS t ON o.type_id = t.id
 LEFT JOIN mileages AS m ON o.mileage_id = m.id
+LEFT JOIN cars AS c ON o.car_id = c.id
 ORDER BY o.date;
